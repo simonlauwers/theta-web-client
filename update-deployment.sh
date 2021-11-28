6c4b664c84aa9361a3097e669e52335621dadc4a
Quinten Verhelst <quinten.verhelst@student.kdg.be>
2021-11-28 11:49:42 +0100

    Add CI-CD, with emphasis on CD

---
+++ update-deployment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RESPONSE_CODE=$(curl --write-out '%{http_code}' --silent --output /dev/null theta-risk.com)
+
+if [[ $RESPONSE_CODE -ne 200  ]]
+then
+  echo "Cluster is down, skipping deployment..."
+  exit 0
+else
+  echo "Cluster is up, deploying..."
+  echo "IMAGE IS"
+  echo $1
+  gcloud container clusters get-credentials --region=europe-west1 risk-cluster
+  if [ $2 = "staging" ]; then
+    echo "Deploying staging env with image $1"
+    kubectl set image deployment/api-gateway "web=$1" -n=staging
+  else
+    echo "Deploying production env with image $1"
+    kubectl set image deployment/api-gateway "weby=$1" -n=production
+  fi
+fi

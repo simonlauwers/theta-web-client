d10c559e46b00a147cb3e240d840ff5f9e8d72af
Nathan Tetroashvili <nathan.tetroashvili@student.kdg.be>
2021-11-28 12:53:50 +0100

    Worked on Login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,15 +1,15 @@
-import { Grid, Stack, Breadcrumbs, Link, Typography } from "@mui/material"
+import { Grid, Breadcrumbs, Link, Typography } from "@mui/material"
 import LoginForm from "./LoginForm";
 
 
-
 const Login = () => {
+
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={6} style={{ height: "100vh", backgroundImage: `url("https://i.pinimg.com/originals/f3/6e/22/f36e22582b470d538a246a87e402e6b1.jpg")`, backgroundSize: "cover" }}>
+        <Grid container>
+            <Grid item xs={0} md={6} lg={6} style={{ height: "100vh", backgroundImage: `url("/media/photos/bgRiskThetaLoginForm.png")`, backgroundSize: "cover" }}>
             </Grid>
-            <Grid item xs={6} style={{ backgroundColor: "#141124", height: "100vh" }}>
-                <Breadcrumbs style={{ marginTop: 15, color: "ghostwhite" }}>
+            <Grid item xs={12} md={6} lg={6} style={{ backgroundColor: "#141124", height: "100vh" }}>
+                <Breadcrumbs style={{ marginLeft: "2em", marginTop: "1em", color: "ghostwhite" }}>
                     <Typography color="ghostwhite"><b>LOGIN</b></Typography>
                     <Link underline="hover" color="inherit" href="/">
                         JOIN
@@ -17,9 +17,7 @@ const Login = () => {
                 </Breadcrumbs>
                 <div style={{ marginTop: "25%", marginLeft: "25%" }}>
                     <h1 style={{ color: "ghostwhite" }}>Welcome back.</h1>
-                    <Stack>
-                        <LoginForm />
-                    </Stack>
+                    <LoginForm />
                 </div>
 
             </Grid>
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,35 +1,87 @@
-import { alpha, styled } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import TextField, { TextFieldProps } from '@mui/material/TextField';
-import { Button, Stack } from '@mui/material';
+import { Button, IconButton, InputAdornment, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
-import { OutlinedInputProps } from '@mui/material/OutlinedInput';
 import WhiteTextField from '../formInputs/WhiteTextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useFormik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
+const validationSchemaLogin = yup.object({
+    email: yup
+        .string()
+        .email('Enter a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .required('Password is required'),
+});
 
-
-
-
+interface LoginValues {
+    email: string,
+    password: string
+}
 
 
 const LoginForm = () => {
     const [showPassword, setShowPassword] = useState<Boolean>(false);
 
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: validationSchemaLogin,
+        onSubmit: (values: LoginValues) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
-        <Stack style={{ maxWidth: "50%" }}>
-            <WhiteTextField
-                label="Email"
-                id="email"
-                variant="filled"
-            />
-            <WhiteTextField
-                label="Password"
-                style={{ marginTop: 25 }}
-                id="email"
-                type="password"
-                variant="filled"
-            />            <Button style={{ marginTop: 25, backgroundColor: "ghostwhite", color: "#141124", fontWeight: "bold" }} variant="contained">Login</Button>
-        </Stack>
+        <form onSubmit={formik.handleSubmit}>
+            <Stack style={{ maxWidth: "50%" }}>
+                <WhiteTextField
+                    label="Email"
+                    id="email"
+                    variant="filled"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                />
+                {formik.errors.email && formik.touched.email ? (<div style={{ color: "white" }}>{"Uh oh... " + formik.errors.email}</div>) : null}
+
+                <WhiteTextField
+                    label="Password"
+                    style={{ marginTop: 25 }}
+                    id="email"
+                    name="password"
+                    type={showPassword ? 'text' : 'password'}
+                    variant="filled"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }}
+                />
+                {formik.errors.password && formik.touched.password ? (<div style={{ color: "white" }}>{"Uh oh... " + formik.errors.password}</div>) : null}
+
+                <Button style={{ marginTop: 25, backgroundColor: "ghostwhite", color: "#141124", fontWeight: "bold" }} type="submit" variant="contained">Login</Button>
+            </Stack>
+        </form >
     );
 
 

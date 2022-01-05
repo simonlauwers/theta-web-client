4ee8491cc12d9dfe68c99e6b1a92114e3f0ee292
Nathan Tetroashvili <nathan.tetroashvili@student.kdg.be>
2022-01-05 21:38:46 +0100

    feat: 🎸 Restyled scenario
    Restyled scenario

--- src/components/scenario-selection/ScenarioPreviewCard.tsx
+++ src/components/scenario-selection/ScenarioPreviewCard.tsx
@@ -1,5 +1,6 @@
 import { Card, Typography } from "@mui/material";
 import React from "react";
+import { backgroundColor } from "../../theme/colors";
 import ScenarioType from "../../types/Game/ScenarioType";
 
 interface ScenarioPreviewCardProps {
@@ -9,16 +10,16 @@ interface ScenarioPreviewCardProps {
 const ScenarioPreviewCard = (props: ScenarioPreviewCardProps) => {
 	return (
 		<Card sx={{
-			minHeight: "100%", maxHeight: "100%", padding: 5, background: "inherit",
-			boxShadow: "inset 0 0 2000px rgba(255, 255, 255, .3)",
+			minHeight: "100%", maxHeight: "100%", padding: 5, backgroundColor: backgroundColor.main
 		}}>
 
 			<Typography onDragStart={(e) => { e.preventDefault(); }} style={{ fontSize: 35, color: "white", fontWeight: 800, textAlign: "center" }}>
 				{props.scenario.name}
 			</Typography>
+			
 			<img style={{
-				marginLeft: "25%", marginTop: 10, marginBottom: 10
-			}} width={"50%"} src={props.scenario.image} alt={props.scenario.name} onDragStart={(e) => { e.preventDefault(); }} />
+				marginLeft: "15%", marginTop: 10, marginBottom: 10
+			}} width={"70%"} src={props.scenario.image} alt={props.scenario.name} onDragStart={(e) => { e.preventDefault(); }} />
 
 		</Card>
 	);
